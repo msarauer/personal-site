0eeb1c3ea757e3852d1143569b4fb51b6b17bfab
Mitchel Sarauer <mitchelsarauer@hotmail.com>
2021-07-22 15:19:47 -0600

    When added, the about section is updated with resume and tech stack.

--- mitchels-site/src/pages/about.js
+++ mitchels-site/src/pages/about.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/about.module.scss"
@@ -11,44 +12,59 @@ const about = () => {
             This is <span> Me</span>.
           </h1>
           <p>
-            I am in the midst of pivoting from a 10+ year career in Civil
-            Engineering and Project Management into a new career as a Full Stack
-            Developer in Calgary, Alberta. As a recent graduate of the
-            Lighthouse Labs Web Development Bootcamp, I have expereince with the
-            following tech stack:
+            Residing in <span>Calgary, Alberta</span>, I am in the midst of
+            pivoting from a <span>10+ year career</span> in Civil Engineering
+            and Project Management into a new career as a Full Stack Developer.
+            As a recent graduate of the{" "}
+            <span>Lighthouse Labs Web Development Bootcamp</span>, I have
+            experience with the following tech stack:
           </p>
-          <dl>
-            <dt>Languages</dt>
-            <dd>Javascript</dd>
-            <dd>HTML</dd>
-            <dd>CSS</dd>
-            <dd>Ruby</dd>
-
-            <dt>Frameworks & Libraries</dt>
-            <dd>React</dd>
-            <dd>JQuery</dd>
-            <dd>Express</dd>
-            <dd>NodeJS</dd>
-            <dd>Rails</dd>
-            <dd>Ajax</dd>
-            <dd>Socket.io</dd>
-
-            <dt>Database</dt>
-            <dd>PostgreSQL</dd>
-
-            <dt>Testing Frameworks</dt>
-            <dd>Mocha/Chai</dd>
-            <dd>Storybook</dd>
-            <dd>Cypress</dd>
-            <dd>Jest</dd>
-          </dl>
+          <div className={styles.stack}>
+            <ul>
+              <h4>Languages</h4>
+              <li>Javascript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Ruby</li>
+            </ul>
+            <ul>
+              <h4>Frameworks & Libraries</h4>
+              <li>React</li>
+              <li>JQuery</li>
+              <li>Express</li>
+              <li>NodeJS</li>
+              <li>Rails</li>
+              <li>Ajax</li>
+              <li>Socket.io</li>
+            </ul>
+            <ul>
+              <h4>Database</h4>
+              <li>PostgreSQL</li>
+            </ul>
+            <ul>
+              <h4>Testing Frameworks</h4>
+              <li>Mocha/Chai</li>
+              <li>Storybook</li>
+              <li>Cypress</li>
+              <li>Jest</li>
+            </ul>
+          </div>
           <p>
             I am excited and eager to move to this new stage of my working life
             and look forward to the interesting challenges that lie ahead!
           </p>
-          <p>
-            Take a look at my <span>resume</span>.
-          </p>
+          <div className={styles.resume}>
+            <p>
+              Take a look at my{" "}
+              <Link
+                className={styles.resumeLink}
+                to="https://resume.creddle.io/resume/eb5udh08rh9"
+              >
+                resume
+              </Link>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
